dbdac9d6c85d49cdd1782ca43e5be1fa25c766d9
mathd1as <matheus.vex@gmail.com>
2021-08-18 16:21:26 -0300

    ensure AuthUseCase throws if no dependency is provided

--- src/domain/usecases/auth-use-case.js
+++ src/domain/usecases/auth-use-case.js
@@ -1,7 +1,7 @@
 const { MissingParamError } = require('../../presentation/utils/errors')
 
 module.exports = class AuthUseCase {
-  constructor ({ loadUserByEmailRepository, encrypter, tokenGenerator }) {
+  constructor ({ loadUserByEmailRepository, encrypter, tokenGenerator } = {}) {
     this.loadUserByEmailRepository = loadUserByEmailRepository
     this.encrypter = encrypter
     this.tokenGenerator = tokenGenerator
--- src/domain/usecases/auth.usecase.spec.js
+++ src/domain/usecases/auth.usecase.spec.js
@@ -89,7 +89,13 @@ describe('Auth UseCase', () => {
   })
 
   test('Should throw if no LoadUserByEmailRepository has no load method', async () => {
-    const sut = new AuthUseCase({ loadUserByEmailRepository: {} })
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@email.com', 'any_password')
+    expect(promise).rejects.toThrow()
+  })
+
+  test('Should throw if no dependency is provided', async () => {
+    const sut = new AuthUseCase({})
     const promise = sut.auth('any_email@email.com', 'any_password')
     expect(promise).rejects.toThrow()
   })
