1f17b24707decafef7fa40d380660ed445a9b577
mathd1as <matheus.vex@gmail.com>
2021-08-18 14:24:25 -0300

    ensure AuthUseCase calls LoadUserByEmailRepository with email

--- src/domain/usecases/auth.usecase.spec.js
+++ src/domain/usecases/auth.usecase.spec.js
@@ -1,6 +1,10 @@
 const { MissingParamError } = require('../../presentation/utils/errors')
 
 class AuthUseCase {
+  constructor (LoadUserByEmailRepository) {
+    this.LoadUserByEmailRepository = LoadUserByEmailRepository
+  }
+
   async auth (email, password) {
     if (!email) {
       return new MissingParamError('email')
@@ -8,19 +12,32 @@ class AuthUseCase {
     if (!password) {
       return new MissingParamError('password')
     }
+    this.LoadUserByEmailRepository.load(email)
   }
 }
 
 describe('Auth UseCase', () => {
-  test('Should throw if no email is provided', async () => {
-    const sut = new AuthUseCase()
-    const promise = sut.auth()
-    expect(promise).rejects.toThrow(new MissingParamError('email'))
-  })
+  // test('Should throw if no email is provided', async () => {
+  //   const sut = new AuthUseCase()
+  //   const promise = sut.auth()
+  //   expect(promise).rejects.toThrow(new MissingParamError('email'))
+  // })
 
-  test('Should throw if no password is provided', async () => {
-    const sut = new AuthUseCase()
-    const promise = sut.auth('any_email@email.com')
-    expect(promise).rejects.toThrow(new MissingParamError('password'))
+  // test('Should throw if no password is provided', async () => {
+  //   const sut = new AuthUseCase()
+  //   const promise = sut.auth('any_email@email.com')
+  //   expect(promise).rejects.toThrow(new MissingParamError('password'))
+  // })
+
+  test('Should call LoadUserByEmailRepository with correct email', async () => {
+    class LoadUserByEmailRepository {
+      async load (email) {
+        this.email = email
+      }
+    }
+    const loadUserByEmailRepositorySpy = new LoadUserByEmailRepository()
+    const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
+    await sut.auth('any_email@email.com', 'any_password')
+    expect(loadUserByEmailRepositorySpy.email).toBe('any_email@email.com')
   })
 })
