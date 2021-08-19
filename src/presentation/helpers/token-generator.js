acfa1422ff995a5bc807b61f699bc095af9b67a7
mathd1as <matheus.vex@gmail.com>
2021-08-19 09:55:08 -0300

    move TokenGenerator to its own file

---
+++ src/presentation/helpers/token-generator.js
@@ -0,0 +1,18 @@
+const { MissingParamError } = require('../utils/errors')
+const jwt = require('jsonwebtoken')
+
+module.exports = class TokenGenerator {
+  constructor (secret) {
+    this.secret = secret
+  }
+
+  async generate (id) {
+    if (!this.secret) {
+      throw new MissingParamError('secret')
+    }
+    if (!id) {
+      throw new MissingParamError('id')
+    }
+    return jwt.sign(id, this.secret)
+  }
+}
