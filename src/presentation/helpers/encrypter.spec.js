4cd8cfb2e104900952f3a4f77cf20921822219db
mathd1as <matheus.vex@gmail.com>
2021-08-19 08:55:34 -0300

    ensure Encrypter throws if no params are provided

--- src/presentation/helpers/encrypter.spec.js
+++ src/presentation/helpers/encrypter.spec.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const Encrypter = require('./encrypter')
+const { MissingParamError } = require('../utils/errors')
 
 const makeSut = () => {
   return new Encrypter()
@@ -21,9 +22,22 @@ describe('Encrypter', () => {
 
   test('Shoul call bcryp with correct values', async () => {
     const sut = makeSut()
-    bcrypt.isValid = false
     await sut.compare('any_value', 'hashed_value')
     expect(bcrypt.value).toBe('any_value')
     expect(bcrypt.hash).toBe('hashed_value')
+  })
+
+  test('Shoul call bcryp with correct values', async () => {
+    const sut = makeSut()
+    await sut.compare('any_value', 'hashed_value')
+    expect(bcrypt.value).toBe('any_value')
+    expect(bcrypt.hash).toBe('hashed_value')
+  })
+
+  test('Shoul throw if no params are provided', async () => {
+    const sut = makeSut()
+
+    expect(sut.compare()).rejects.toThrow(new MissingParamError('value'))
+    expect(sut.compare('any_value')).rejects.toThrow(new MissingParamError('hash'))
   })
 })
