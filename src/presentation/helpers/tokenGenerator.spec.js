e6f5b4fb2412e3fc2895be4205e1b7abd50783cf
mathd1as <matheus.vex@gmail.com>
2021-08-19 09:52:03 -0300

    ensure TokenGenerator throws if no id is provided

--- src/presentation/helpers/tokenGenerator.spec.js
+++ src/presentation/helpers/tokenGenerator.spec.js
@@ -10,6 +10,9 @@ class TokenGenerator {
     if (!this.secret) {
       throw new MissingParamError('secret')
     }
+    if (!id) {
+      throw new MissingParamError('id')
+    }
     return jwt.sign(id, this.secret)
   }
 }
@@ -42,10 +45,17 @@ describe('Email Validator', () => {
     expect(jwt.secret).toBe(sut.secret)
   })
 
-  test('Should throw if no secret or id are provided', async () => {
+  test('Should throw if no secret are provided', async () => {
     const sut = new TokenGenerator()
     const promise = sut.generate('any_id')
 
     expect(promise).rejects.toThrow(new MissingParamError('secret'))
+  })
+
+  test('Should throw if no id are provided', async () => {
+    const sut = makeSut()
+    const promise = sut.generate()
+
+    expect(promise).rejects.toThrow(new MissingParamError('id'))
   })
 })
