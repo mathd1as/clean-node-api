7ac415115a19702b1e48fd62388ab2352ceb8b14
mathd1as <matheus.vex@gmail.com>
2021-08-19 09:25:01 -0300

    ensure TokenGenerator returns null if no params are provided

---
+++ src/presentation/helpers/tokenGenerator.spec.js
@@ -0,0 +1,14 @@
+class TokenGenerator {
+  async generate (id) {
+    return null
+  }
+}
+
+describe('Email Validator', () => {
+  test('Should return null if JWT returns null', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+
+    expect(token).toBe(null)
+  })
+})
