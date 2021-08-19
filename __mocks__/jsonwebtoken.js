303a74265b7b86a9b1d88045a3049d9f694fd08f
mathd1as <matheus.vex@gmail.com>
2021-08-19 09:36:04 -0300

    ensure TokenGenerator returns token if JWT returns token

---
+++ __mocks__/jsonwebtoken.js
@@ -0,0 +1,6 @@
+module.exports = {
+  token: 'any_token',
+  sign (id, scret) {
+    return this.token
+  }
+}
--- src/presentation/helpers/tokenGenerator.spec.js
+++ src/presentation/helpers/tokenGenerator.spec.js
@@ -1,14 +1,24 @@
+const jwt = require('jsonwebtoken')
+
 class TokenGenerator {
   async generate (id) {
-    return null
+    return jwt.sign(id, 'secret')
   }
 }
 
 describe('Email Validator', () => {
   test('Should return null if JWT returns null', async () => {
     const sut = new TokenGenerator()
+    jwt.token = null
     const token = await sut.generate('any_id')
 
     expect(token).toBe(null)
+  })
+
+  test('Should returns a token if JWT returns token', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+
+    expect(token).toBe(jwt.token)
   })
 })
